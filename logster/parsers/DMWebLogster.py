aff3088347a817546f3a77bd59cd33701119fe5c
Andrew Seales <andrew.seales@ed.ac.uk>
2019-07-15 13:09:48 +0100

    Filter off GetCapabilities from WMS Service

--- logster/parsers/DMWebLogster.py
+++ logster/parsers/DMWebLogster.py
@@ -56,7 +56,7 @@ class DMWebLogster(LogsterParser):
         self.regRegister = re.compile('.*PUT /api/user/register HTTP/\d.\d" (?P<code>\d+) .* (?P<response>\d+) [\w\d-]+ .$')
         self.regDownloads = re.compile('.*POST (/roam/api/download/orders|/datadownload/submitorder).* HTTP/\d.\d" (?P<code>\d+) .* (?P<response>\d+) [\w\d-]+ .$')
         self.regMapproxy = re.compile('.*GET /mapproxy/wmsMap.* HTTP/\d.\d" (?P<code>\d+) .* (?P<response>\d+) [\w\d-]+ .$')
-        self.regMapproxyWms = re.compile('.*GET /mapproxy/wms/.* HTTP/\d.\d" (?P<code>\d+) .* (?P<response>\d+) [\w\d-]+ .$')
+        self.regMapproxyWms = re.compile('.*GET /mapproxy/wms/.*GetMap.* HTTP/\d.\d" (?P<code>\d+) .* (?P<response>\d+) [\w\d-]+ .$')
 
 
     def parse_line(self, line):
